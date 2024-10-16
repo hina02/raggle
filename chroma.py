760f04213c03c0914f4c0a6bb39c119059c4dccc
hina02 <hina02limited@gmail.com>
2024-10-17 08:07:34 +0900

    adjust text splitter

--- chroma.py
+++ chroma.py
@@ -1,24 +1,42 @@
 import chromadb
+from langchain.embeddings import OpenAIEmbeddings
 from langchain_chroma import Chroma
 from langchain_core.documents import Document
 
+embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
 
+
+# def from_documents(
+#     cls: Type[Chroma],
+#     documents: List[Document],
+#     embedding: Optional[Embeddings] = None,
+#     ids: Optional[List[str]] = None,
+#     collection_name: str = _LANGCHAIN_DEFAULT_COLLECTION_NAME,
+#     persist_directory: Optional[str] = None,
+#     client_settings: Optional[chromadb.config.Settings] = None,
+#     client: Optional[chromadb.ClientAPI] = None,  # Add this line
+#     collection_metadata: Optional[Dict] = None,
+#     **kwargs: Any,
+# ) -> Chroma:
 class ChromaManager:
-    def __init__(self, client, collection_name):
-
-        persistent_client = chromadb.PersistentClient()
-        collection = persistent_client.get_or_create_collection("collection_name")
-        collection.add(ids=["1", "2", "3"], documents=["a", "b", "c"])
-
-        vector_store_from_client = Chroma(
-            client=persistent_client,
-            collection_name="collection_name",
+    def __init__(self, collection_name: str):
+        self.vector_store = Chroma(
+            collection_name=collection_name,
             embedding_function=embeddings,
+            collection_metadata={"hnsw:space": "cosine"},
         )
 
-    def add_documents(self, documents):
-        pass
+    def add_documents(self, documents: list[Document]) -> list[str]:
+        ids = self.vector_store.add_documents(documents)
+        print(f"documents added to chroma {len(ids)} / {len(documents)}.")
+        return ids
 
-    def query(self, query):
-        pass
-    
+    def query(self, query: str, filter: dict, k: int = 4) -> list[tuple[Document, float]]:
+        results = self.vector_store.similarity_search_by_vector_with_relevance_scores(
+            embedding=embeddings.embed_query(query),
+            k=k,
+            filter=filter,
+            # where_document={"$contains": {"text": "hello"}},
+        )
+        # results = self.vector_store.max_marginal_relevance_search_by_vector
+        return results
--- main.py
+++ main.py
@@ -1,20 +1,24 @@
-import json
-import sys
-import os
 import datetime
+import json
+import os
+import sys
 
 from dotenv import load_dotenv
 from langchain import callbacks
-from pydantic import BaseModel
+from langchain_community.document_loaders import PDFPlumberLoader, PyPDFLoader
 from langchain_core.documents import Document
-from langchain_community.document_loaders import PyPDFLoader, PDFPlumberLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter, NLTKTextSplitter, SpacyTextSplitter
-from langchain_openai.embeddings.base import OpenAIEmbeddings   # batch使っていない
+from langchain_openai.embeddings.base import OpenAIEmbeddings  # batch使っていない
+from langchain_text_splitters import (
+    CharacterTextSplitter,
+    NLTKTextSplitter,
+    RecursiveCharacterTextSplitter,
+    SpacyTextSplitter,
+)
+from pydantic import BaseModel
 
-
-#==============================================================================
+# ==============================================================================
 # !!! 警告 !!!: 以下の変数を変更しないでください。
-#==============================================================================
+# ==============================================================================
 model = "gpt-4o-mini"
 pdf_file_urls = [
     "https://storage.googleapis.com/gg-raggle-public/competitions/29676d73-5675-4278-b1a6-d4a9fdd0a0ba/dataset/Architectural_Design_Service_Contract.pdf",
@@ -30,49 +34,48 @@ pdf_file_urls = [
     "https://storage.googleapis.com/gg-raggle-public/competitions/29676d73-5675-4278-b1a6-d4a9fdd0a0ba/dataset/software_development_outsourcing_contracts.pdf",
     "https://storage.googleapis.com/gg-raggle-public/competitions/29676d73-5675-4278-b1a6-d4a9fdd0a0ba/dataset/Technical_Verification_(PoC)_Contract.pdf",
 ]
-#==============================================================================
+# ==============================================================================
 
 
-#==============================================================================
+# ==============================================================================
 # この関数を編集して、あなたの RAG パイプラインを実装してください。
 # !!! 注意 !!!: デバッグ過程は標準出力に出力しないでください。
-#==============================================================================
-KEYWORDS = []   # from Sudachi, to hybrid search(vetor and BM25) in chroma
+# ==============================================================================
+KEYWORDS = []  # from Sudachi, to hybrid search(vetor and BM25) in chroma
 
 # リクエスト5回制限のため、Batch API を使用する。
-# ①　テキスト変換（代名詞、年月日）
-# ②　Embedding　
+# ①　テキスト変換（代名詞、年月日） : 時間かかるなら没か。
+# ②　Embedding
 # ③　クエリ変換
 # ④　ReRank
 # ⑤　結果出力
 
-class Paragraph(BaseModel):
-    paragraph_id: int
-    text: str
-    embedding_content: str  # 代名詞を明確な名詞に変換
-    # embedding: list[float]
 
 class Article(BaseModel):
     article_id: int
+    title: str
     text: str
     # embedding: list[float]
-    paragraphs: list[Paragraph]
+    related_articles: list[int]  # list of article_id
 
-class Party(BaseModel): # colletion2
+
+class Party(BaseModel):  # colletion2
     name: str
     address: str
     contact: str
     delegate: str
     # embedding: list[float]
 
-class ContractAgreement(BaseModel): #collection1
+
+class ContractAgreement(BaseModel):  # collection1
     title: str
     text: str
     PartyA: Party
     PartyB: Party
-    created_at: str # 可能ならdatetime
+    created_at: str  # 可能ならdatetime
     # embedding: list[float]
     articles: list[Article]
+
 
 # 1st LLM (analyse query [contract(type, date), party, , etc.])
 Prompt = """
@@ -84,40 +87,58 @@ Prompt = """
  }
 """
 
+
 def load_pdf_files(dir_path: str = "dataset"):
     pdf_files = []
     for file_name in os.listdir(dir_path):
-        if file_name.endswith('.pdf'):
+        if file_name.endswith(".pdf"):
             pdf_files.append(os.path.join(dir_path, file_name))
     return pdf_files
 
+
 def load_pdf(path: str) -> list[Document]:
-    pdf_loader = PyPDFLoader(file_path=path)
-    separators = []
-    article_pattern = r"第[ 　]*[０-９0-9一二三四五六七八九十百]+[ 　]*条"
-    section_pattern = r"[ 　]*[０-９0-9]+[ 　]*\.[ 　]*"
+    separators = ["\n+第\s*[０-９0-9一二三四五六七八九十百]+\s*条", "\n+[（【]\s*別紙\s*[）】]\n+"]
 
-    text_splitter = RecursiveCharacterTextSplitter(separators = separators, is_separator_regex = True)
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=100,
+        chunk_overlap=0,
+        separators=separators,
+        is_separator_regex=True,
+    )
+
+    # pdf_loader = PyPDFLoader(file_path=path)
+    pdf_lumber_loader = PDFPlumberLoader(file_path=path)  # TODO Check
+    documents = pdf_lumber_loader.load_and_split(text_splitter=text_splitter)
+
+    # document.page_contentがseparatorsにマッチしていない場合、一つ前のdocument.page_contentに結合する。
+    # [０-９0-9一二三四五六七八九十百]を抜き出し、intに変換し、article_idに渡す。
+    for document in documents:
+        print(document.id)
+        print(document.metadata)
+        print(document.page_content)
+
+    # 条のタイトルを抜き出し、article_titleに渡す。
+
+    # 関連する条項（前項は問題なし。第2条第1項等を抜き出し、related_articlesに渡す。前条の場合も同様。）
+    # （存続条項は除外する）
+
+    return documents
 
 
-    pdf_table_loader = PDFPlumberLoader(file_path=path) # TODO Check
-    pages = pdf_loader.load_and_split(text_splitter=text_splitter)
-
-
-    pdf_files = pdf_loader.load_files(pdf_file_urls)
-    return pdf_files
+load_pdf(pdf_file_urls[0])
 
 
 def rag_implementation(question: str) -> str:
     # once load pdf_files
-
-    
-#==============================================================================
+    pass
 
 
-#==============================================================================
+# ==============================================================================
+
+
+# ==============================================================================
 # !!! 警告 !!!: 以下の関数を編集しないでください。
-#==============================================================================
+# ==============================================================================
 def main(question: str):
     with callbacks.collect_runs() as cb:
         result = rag_implementation(question)
@@ -127,13 +148,13 @@ def main(question: str):
     print(json.dumps(output))
 
 
-if __name__ == "__main__":
-    load_dotenv()
+# if __name__ == "__main__":
+#     load_dotenv()
 
-    if len(sys.argv) > 1:
-        question = sys.argv[1]
-        main(question)
-    else:
-        print("Please provide a question as a command-line argument.")
-        sys.exit(1)
-#==============================================================================
+#     if len(sys.argv) > 1:
+#         question = sys.argv[1]
+#         main(question)
+#     else:
+#         print("Please provide a question as a command-line argument.")
+#         sys.exit(1)
+# ==============================================================================
